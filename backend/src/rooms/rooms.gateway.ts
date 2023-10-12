ffcd62f354606349eb5168f2d3731b067526abf9
leomozzer <mozzer.ferraz@gmail.com>
2023-10-11 23:22:34 -0300

    started rooms with websocket

---
+++ backend/src/rooms/rooms.gateway.ts
@@ -0,0 +1,39 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+  OnGatewayConnection,
+  OnGatewayDisconnect
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  namespace: '/rooms', transports: ['websocket'], cors: {
+    origin: true,
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }
+})
+export class RoomsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  async handleConnection(client: Socket) {
+    console.log(`Client ${client.id} connected on /rooms`)
+  }
+
+  async handleDisconnect(client: Socket) {
+    console.log(`Client ${client.id} disconneted of /rooms`)
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): string {
+    console.log(payload);
+    client.emit('message', 'Hello from websocket server');
+    return 'Hello world!';
+  }
+}
