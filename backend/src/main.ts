697b1b8e007962d4866c6576d794f9dbface8ab8
leomozzer <mozzer.ferraz@gmail.com>
2023-10-19 23:59:31 -0300

    Added cors due to the frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    'origin': 'http://localhost'
+  })
   await app.listen(3000);
 }
 bootstrap();
