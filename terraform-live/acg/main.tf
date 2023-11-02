d9ac9fe16cdadcff498b34576164e11578325685
leomozzer <mozzer.ferraz@gmail.com>
2023-11-02 18:02:28 -0300

    Added mongodb container - fixed subent

--- terraform-live/acg/main.tf
+++ terraform-live/acg/main.tf
@@ -47,18 +47,12 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  lifecycle {
-    ignore_changes = [
-      subnet
-    ]
-  }
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = local.snet_name
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.id
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
