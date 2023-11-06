38910ddedaa43b8e03390f570cf210153c7d5def
leomozzer <mozzer.ferraz@gmail.com>
2023-11-06 20:50:03 -0300

    Adding service delegation

--- terraform-live/acg/main.tf
+++ terraform-live/acg/main.tf
@@ -26,6 +26,13 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  delegation {
+    name = "delegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
 
 resource "azurerm_network_security_group" "nsg" {
