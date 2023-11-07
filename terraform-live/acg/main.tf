d208784b23d8d9925892ac7166a5366cbe486b31
leomozzer <mozzer.ferraz@gmail.com>
2023-11-07 13:35:39 -0300

    Added nginx image just for testing

--- terraform-live/acg/main.tf
+++ terraform-live/acg/main.tf
@@ -145,30 +145,29 @@ resource "azurerm_container_group" "backend" {
   }
 
   container {
-    name = "nginx"
-    image = "nginx:lastest"
-    cpu = "1.0"
+    name   = "nginx"
+    image  = "nginx:lastest"
+    cpu    = "1.0"
     memory = "2.0"
-  }
 
-  # container {
-  #   name   = "backend"
-  #   image  = "${data.azurerm_container_registry.acr.login_server}/backend:latest"
-  #   cpu    = "1.0"
-  #   memory = "2.0"
+    # container {
+    #   name   = "backend"
+    #   image  = "${data.azurerm_container_registry.acr.login_server}/backend:latest"
+    #   cpu    = "1.0"
+    #   memory = "2.0"
 
-  #   environment_variables = {
-  #     MONGODB_HOST                 = azurerm_container_group.mongodb.ip_address
-  #     MONGO_INITDB_DATABASE        = "mydb"
-  #     MONGO_INITDB_ROOT_USERNAME   = "root"
-  #     MONGO_INITDB_ROOT_PASSWORD   = "example"
-  #     NODE_ENV                     = "development"
-  #     JWT_TOKEN                    = "ABC123456"
-  #     JWT_TOKEN_EXPIRATION_SECONDS = "144000s"
-  #     APP_PORT                     = 80
-  #   }
+    #   environment_variables = {
+    #     MONGODB_HOST                 = azurerm_container_group.mongodb.ip_address
+    #     MONGO_INITDB_DATABASE        = "mydb"
+    #     MONGO_INITDB_ROOT_USERNAME   = "root"
+    #     MONGO_INITDB_ROOT_PASSWORD   = "example"
+    #     NODE_ENV                     = "development"
+    #     JWT_TOKEN                    = "ABC123456"
+    #     JWT_TOKEN_EXPIRATION_SECONDS = "144000s"
+    #     APP_PORT                     = 80
+    #   }
 
-  #   commands = ["npm", "run", "start:prod"]
+    #   commands = ["npm", "run", "start:prod"]
 
     ports {
       port     = 80
